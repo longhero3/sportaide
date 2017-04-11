f704775329cd731baa1cb54e85688df75283651b
Long Dang <long.dang@gmail.com>
2017-04-11 19:59:26 +1000

    Fix compile issue

--- app/assets/javascripts/react-components/compiled_components.js
+++ app/assets/javascripts/react-components/compiled_components.js
@@ -1311,6 +1311,55 @@ function customDistanceToMouse(pt, mousePos, markerProps) {
 
 ;'use strict';
 
+function getMarkerHolderStyle(size, origin) {
+  var left = -size.width * origin.x;
+  var top = -size.height * origin.y;
+  return {
+    position: 'absolute',
+    width: size.width,
+    height: size.height,
+    left: left,
+    top: top,
+    cursor: 'pointer'
+  };
+}
+
+function getMarkerStyle(size, origin) {
+  var sizeOriginX = size.width * origin.x;
+  var sizeOriginY = size.height * origin.y;
+
+  return {
+    position: 'absolute',
+    width: size.width,
+    height: size.height,
+    left: 0,
+    top: 0,
+    willChange: 'transform', // it looks like this setting make firefox random marker movements smaller
+    backgroundSize: size.width + 'px ' + size.height + 'px',
+    backgroundRepeat: 'no-repeat',
+    // transition: 'transform 0.25s ease',
+    transition: 'transform 0.25s cubic-bezier(0.485, 1.650, 0.545, 0.835)',
+    WebkitTransition: '-webkit-transform 0.25s cubic-bezier(0.485, 1.650, 0.545, 0.835)',
+    transformOrigin: sizeOriginX + 'px ' + sizeOriginY + 'px',
+    WebkitTransformOrigin: sizeOriginX + 'px ' + sizeOriginY + 'px'
+  };
+}
+
+var textStyle = {
+  width: '100%',
+  textAlign: 'center',
+  marginTop: 10,
+  fontWeight: 'bold',
+  fontSize: '18px',
+  color: 'black'
+};
+
+function getMarkerTextStyle() {
+  return textStyle;
+}
+
+;'use strict';
+
 /*
  * marker visual parameters
  * image param is more prior than imageClass if both defined
@@ -2530,55 +2579,7 @@ var store = (0, _redux.createStore)(TodoApp, (0, _redux.applyMiddleware)(_reduxT
 
 (0, _reactDom.render)(_react2.default.createElement(Root, { store: store }), document.getElementById('dashboard'));
 
-
-function getMarkerHolderStyle(size, origin){
-  const left = -size.width * origin.x;
-  const top = -size.height * origin.y;
-  return {
-    position: 'absolute',
-    width: size.width,
-    height: size.height,
-    left: left,
-    top: top,
-    cursor: 'pointer'
-  };
-}
-
-function getMarkerStyle(size, origin) {
-  const sizeOriginX = size.width * origin.x;
-  const sizeOriginY = size.height * origin.y;
-
-  return {
-    position: 'absolute',
-    width: size.width,
-    height: size.height,
-    left: 0,
-    top: 0,
-    willChange: 'transform', // it looks like this setting make firefox random marker movements smaller
-    backgroundSize: `${size.width}px ${size.height}px`,
-    backgroundRepeat: 'no-repeat',
-    // transition: 'transform 0.25s ease',
-    transition: 'transform 0.25s cubic-bezier(0.485, 1.650, 0.545, 0.835)',
-    WebkitTransition: '-webkit-transform 0.25s cubic-bezier(0.485, 1.650, 0.545, 0.835)',
-    transformOrigin: `${sizeOriginX}px ${sizeOriginY}px`,
-    WebkitTransformOrigin: `${sizeOriginX}px ${sizeOriginY}px`
-  };
-}
-
-var textStyle = {
-  width: '100%',
-  textAlign: 'center',
-  marginTop: 10,
-  fontWeight: 'bold',
-  fontSize: '18px',
-  color: 'black'
-};
-
-function getMarkerTextStyle() {
-  return textStyle;
-}
-
-;'use strict';
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
