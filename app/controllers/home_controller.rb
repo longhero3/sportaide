6a91259ea0b1c698cb5143e5536690e9b05b5a29
Long Dang <long.dang@gmail.com>
2017-05-13 21:20:15 +1000

    Add course tile to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     responses = HTTParty.get('http://newsapi.org/v1/articles?source=the-sport-bible&sortBy=top&apiKey=f5035499dacf4b50adfc7123c5e3fe2f')
     @articles = []
+    @courses = Course.all
     if responses["status"]
       @articles = responses["articles"]
     end
